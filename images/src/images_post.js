c9fb338fbf0ae937bbff33af1ea9865433c84845
Patrick Kissling <patrick.kissling@de.bosch.com>
2021-03-31 09:02:57 +0200

    Add cors headers in response

--- images/src/images_post.js
+++ images/src/images_post.js
@@ -9,6 +9,13 @@ exports.handler = async (event, context) => {
   const id = context.awsRequestId
   const expiresInSeconds = 60 * 5 // 5 mins
 
+  // cors
+  var corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,POST"
+    }
+
   const url = await s3.getSignedUrl('putObject', {
     "Bucket": bucket,
     "Key": id,
@@ -16,6 +23,7 @@ exports.handler = async (event, context) => {
   })
 
   return {
+    headers: corsHeaders,
     statusCode: 200,
     body: JSON.stringify({ url, id })
   }
