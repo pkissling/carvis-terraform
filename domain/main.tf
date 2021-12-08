11f3517e1fa520c868183ece376b15aa9bd0338c
Patrick Kissling <patrick.kissling@gmx.de>
2021-12-08 22:25:08 +0100

    Add mail module

--- domain/main.tf
+++ domain/main.tf
@@ -1,6 +1,7 @@
 resource "aws_route53_zone" "this" {
   name = var.domain
 }
+
 resource "aws_route53_record" "apex" {
   zone_id = aws_route53_zone.this.zone_id
   name    = var.env == "live" ? "" : var.env
@@ -8,6 +9,7 @@ resource "aws_route53_record" "apex" {
   ttl     = "300"
   records = var.env == "live" ? [var.website_host_ip] : [var.website_cname]
 }
+
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.this.zone_id
   name    = var.env == "live" ? "www" : "www.${var.env}"
@@ -31,4 +33,23 @@ resource "aws_route53_record" "certificate_validation" {
   type    = var.certificate_validation_record_type
   records = [var.certificate_validation_record_value]
   ttl     = 60
+}
+
+resource "aws_route53_record" "mail_identity_validation" {
+  count   = var.env == "live" ? 3 : 0
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "${element(var.mail_domain_validation_dkim_tokens, count.index)}._domainkey"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(var.mail_domain_validation_dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
+# Record to allow receiving mails
+resource "aws_route53_record" "mail_receiver_validation" {
+  count   = var.env == "live" ? 1 : 0
+  zone_id = aws_route53_zone.this.zone_id
+  name    = var.domain
+  type    = "MX"
+  ttl     = "300"
+  records = ["10 inbound-smtp.eu-west-1.amazonaws.com"] # Static value
 }
---
+++ mail/variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "to_mail" {
+  type = string
+}
